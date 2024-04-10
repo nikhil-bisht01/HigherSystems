4d6c4cc555d6311531684fba3335c962478939ee
nikhil-bisht01 <163300749+nikhil-bisht01@users.noreply.github.com>
2024-04-10 17:30:29 +0530

    update

--- src/Signup.js
+++ src/Signup.js
@@ -1,78 +1,164 @@
-import React, { useState } from 'react';
-import Navbar from './Navbar';
+import React, { useState } from "react";
+import Navbar from "./Navbar";
 import "./Signup.css";
-function SignupPage() {
-  const [Firstname, setName] = useState("");
-  const [Lastname, setLastname] = useState("");
-  const [email, setEmail] = useState('');
-  const [phoneno, setPhone] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  
-  const handleSignup = (e) => {
-    e.preventDefault();
-    // Handle signup logic here
-    if(password===confirmPassword){
-      alert("Signup Successful");
 
-    }
-    else{
+function SignupPage() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNo, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
       alert("Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await fetch("your-backend-api-url/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          phoneNo,
+          password,
+        }),
+      });
+
+      if (response.ok) {
+        alert("Signup Successful");
+        // Redirect the user to the dashboard or login page
+      } else {
+        const data = await response.json();
+        if (response.status === 409) {
+          alert("User already exists in the database");
+        } else {
+          alert("Signup failed");
+        }
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+      alert("Signup failed");
     }
   };
-  
+
   return (
-    <div >
-      <img className='login-img' src={require('./login2.png')} alt='img' />
-      <img className='signup-img' src={require('./signin.png')} alt='img' />
-      <div className='welcome'><h1>Welcome</h1>
-     </div>
-     <p style={{fontSize:'1.3rem', position:'absolute',right:'56%',top:'18%', color:'#ffffff'}}>Dont have an account? Create your account,<br/> it takes less than a minute</p>
-      <div className='login-form'>
-        <div className='log-btn' style={{ display: 'flex', justifyContent: 'space-between',fontSize:'1.5rem' }}>
-          <h1 className=''>Sign Up</h1>
+    <div>
+      <div>
+        <img
+          className="login-img"
+          src={require("./login2.png")}
+          alt="login image"
+        />
+        <img
+          className="signup-img"
+          src={require("./signin.png")}
+          alt="signup image"
+        />
+        <div className="welcome">
+          <h1>Welcome</h1>
         </div>
-        <form onSubmit={handleSignup}>
-          <div></div>
-         <div className='property' >
-         <label className='lable'>
-            First Name
-          </label>
-          <input className='log-email' style={{ border: '1px solid black', width:'200px' }} type="text" value={email} onChange={e => setEmail(e.target.value)} required />
-          <label className='lable'>
-            Last Name
-          </label>
-          <input className='log-email' style={{ border: '1px solid black',width:'200px' }} type='tel' value={phoneno} onChange={e => setPhone(e.target.value)} required />
-          
-         </div>
-         <div className='property'>
-
-          <label className='lable'>
-            E-mail *
-          </label>
-          <input className='log-email' style={{ border: '1px solid black', width:'305px' }} type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-          <label className='lable'>
-            Phone Number*
-          </label>
-          <input className='log-email' style={{ border: '1px solid black',width:'305px' }} type='tel' value={phoneno} onChange={e => setPhone(e.target.value)} required />
-         </div>
-          
-          <label className='lable'>
-            Password:
-          </label>
-          <input className='log-chg-pass' type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-
-          <label className='con-pass-lable'>
-            Confirm Password:
-          </label>
-          <input className='log-chg-pass' type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} required />
-         
-          <button className='log-sub'>Sign Up</button>
-        </form>
-        {/* Already have an account? Sign In Link */}
-        <p className="sign-up">Already have an account?<a className='signup-link' href="/login">Sign in</a></p>
+        <p
+          style={{
+            fontSize: "1.3rem",
+            position: "absolute",
+            right: "56%",
+            top: "18%",
+            color: "#ffffff",
+          }}
+        >
+          Don't have an account? Create your account,
+          <br /> it takes less than a minute
+        </p>
+        <div className="login">
+          <form onSubmit={handleSignup}>
+            <div className="child">
+              <div>
+                <label className="label">First Name</label>
+                <input
+                  className="log-email"
+                  style={{ border: "1px solid black", width: "200px" }}
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label className="label">Last Name</label>
+                <input
+                  className="log-email"
+                  style={{ border: "1px solid black", width: "200px" }}
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="child">
+              <div>
+                <label className="label">E-mail *</label>
+                <input
+                  className="log-email"
+                  style={{ border: "1px solid black", width: "305px" }}
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label className="label">Phone Number*</label>
+                <input
+                  className="log-email"
+                  style={{ border: "1px solid black", width: "305px" }}
+                  type="tel"
+                  value={phoneNo}
+                  onChange={(e) => setPhone(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="child">
+              <div>
+                <label className="label">Password:</label>
+                <input
+                  className="log-emal"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label className="label">Confirm Password:</label>
+                <input
+                  className="log-email"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="child4">
+              <button className="log-sub" type="submit">
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <Navbar/>
+      <Navbar />
     </div>
   );
 }
