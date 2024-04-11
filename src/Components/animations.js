2f566446a5c1120c36a092850f34b705385952ec
nikhil-bisht01 <163300749+nikhil-bisht01@users.noreply.github.com>
2024-04-11 15:22:23 +0530

    changes

--- src/Components/animations.js
+++ src/Components/animations.js
@@ -1,8 +1,8 @@
 function myFunction() {
   var imglarge = document.querySelector(".image-cont img")
-  var container= document.querySelector("#play")
+  var container= document.querySelector("#box")
 imglarge.addEventListener("mouseenter", function () {
-  container.style.opacity = 1;
+  container.style.opacity = 0.8;
   container.style.height = "350px"
   container.style.transition = "all 1s ease-in-out";
 })
