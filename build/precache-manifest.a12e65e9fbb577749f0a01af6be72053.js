e08a84b1cc435662e9d51835f6fbfc9e4c159daa
nikhil-bisht01 <163300749+nikhil-bisht01@users.noreply.github.com>
2024-04-27 15:37:32 +0530

    build

---
+++ build/precache-manifest.a12e65e9fbb577749f0a01af6be72053.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bd9de899ab59639b0f5e77b920c757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5da9fef37f318220f7a2",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "8885cef96ccd0aa62c74",
+    "url": "/static/css/main.58dadd62.chunk.css"
+  },
+  {
+    "revision": "5da9fef37f318220f7a2",
+    "url": "/static/js/2.a3ac5812.chunk.js"
+  },
+  {
+    "revision": "d312d89439d9d8ce6fadd850a761c31d",
+    "url": "/static/js/2.a3ac5812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae233968f475b5dc4d45",
+    "url": "/static/js/3.b87e62c3.chunk.js"
+  },
+  {
+    "revision": "8885cef96ccd0aa62c74",
+    "url": "/static/js/main.9157a6a9.chunk.js"
+  },
+  {
+    "revision": "be69905a1c0839fbeeb3",
+    "url": "/static/js/runtime-main.4e098b4d.js"
+  },
+  {
+    "revision": "701018209d7f8eb0b219b504e8fbc33d",
+    "url": "/static/media/About1.70101820.jpg"
+  },
+  {
+    "revision": "a9cf38f23191fe8be1f4e0da7af2351a",
+    "url": "/static/media/Cloud.a9cf38f2.jpg"
+  },
+  {
+    "revision": "9101ff50368485d8ebce0de19f49d35f",
+    "url": "/static/media/Digital.9101ff50.jpg"
+  },
+  {
+    "revision": "9d725a8c9e7a57a4b62e7924b605c3b3",
+    "url": "/static/media/ERP.9d725a8c.webp"
+  },
+  {
+    "revision": "264378015b1f34d09b859929deed3e98",
+    "url": "/static/media/Employee.26437801.webp"
+  },
+  {
+    "revision": "14a0e86c8cf4ce59187a60b1fed06695",
+    "url": "/static/media/Home.14a0e86c.png"
+  },
+  {
+    "revision": "6192be411e01e3a742e67130433c254b",
+    "url": "/static/media/assets (2).6192be41.svg"
+  },
+  {
+    "revision": "2ce859aab716ea046fb7e49bc0d57e6e",
+    "url": "/static/media/avtar2.2ce859aa.gif"
+  },
+  {
+    "revision": "cc8e0d8a0a136b97989de717306ea7e0",
+    "url": "/static/media/contactuspage1.cc8e0d8a.jpg"
+  },
+  {
+    "revision": "86debfd2413b214e1eba08ead3f2137a",
+    "url": "/static/media/content-management.86debfd2.svg"
+  },
+  {
+    "revision": "59fcc628f940bb40f7e7fd573b4f54c6",
+    "url": "/static/media/doucment.59fcc628.svg"
+  },
+  {
+    "revision": "d2e461315b5b723c13b55d838d21bd3a",
+    "url": "/static/media/elearning.d2e46131.svg"
+  },
+  {
+    "revision": "4164b0dc127b0506d2246cee33d0aba4",
+    "url": "/static/media/enterprisre.4164b0dc.svg"
+  },
+  {
+    "revision": "e8a26b9731a689b2cea9d458ae9f3692",
+    "url": "/static/media/human resource.e8a26b97.svg"
+  },
+  {
+    "revision": "2a15d6dd69968a78f6aaf1009e443083",
+    "url": "/static/media/inventory.2a15d6dd.svg"
+  },
+  {
+    "revision": "a8b8da61ca09a9e25b1faba612386a94",
+    "url": "/static/media/location.a8b8da61.svg"
+  },
+  {
+    "revision": "ee45858382849eed55aabe4e3e7c399c",
+    "url": "/static/media/login-img.ee458583.png"
+  },
+  {
+    "revision": "236e4e4d2114de3fe981e1856534313d",
+    "url": "/static/media/logo.236e4e4d.png"
+  },
+  {
+    "revision": "def74be1ae3a7e5f1140acb9c14c1e60",
+    "url": "/static/media/mailbox.def74be1.svg"
+  },
+  {
+    "revision": "08d6bc7123f6019598a9fb6eade15f08",
+    "url": "/static/media/remove.08d6bc71.svg"
+  },
+  {
+    "revision": "464f14df15320042e1482eb08cd1b300",
+    "url": "/static/media/signin.464f14df.png"
+  },
+  {
+    "revision": "42be4cab603d2408ad47373156582df9",
+    "url": "/static/media/solution4.42be4cab.png"
+  }
+]);
